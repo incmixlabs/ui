c8c284bda849fcda230aa868d7fc643bbf00154c
uma-uc <uma.apple123@gmail.com>
2025-01-23 19:59:13 -0800

    chore: wip

--- frontend/ui/src/components/table/TableHeadCell.tsx
+++ frontend/ui/src/components/table/TableHeadCell.tsx
@@ -24,7 +24,7 @@ export const TableHeadCell = forwardRef<HTMLTableCellElement, TableHeadCellProps
     const { theme: headTheme } = useTableHeadContext();
     const tableContext = useTableContext();
     const theme = mergeDeep(headTheme.cell, customTheme);
-    const padding = tableContext.tight ? theme.tight : theme.base;
+    const padding = tableContext.tight ? theme.padding.tight : theme.padding.base;
     console.log("Padding ", padding);
     console.log("is tight", tableContext.tight);
     return (
