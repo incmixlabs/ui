262534e3e15ba48820a590cf06ce4f9003617b80
naymurdev <naymurrahman178@gmail.com>
2025-02-21 22:59:00 +0600

    feat(task-page): add kanban cards details drawer/sidebar

--- apps/fihi-app/src/Main.tsx
+++ apps/fihi-app/src/Main.tsx
@@ -1,10 +1,10 @@
-import "@incmix/ui/styles/global.css"
+import "@incmix/ui/styles/global.css";
 // import "./instrument"
-import { NuqsReactAdaptor } from "@incmix/ui/data-table"
-import React from "react"
-import ReactDOM from "react-dom/client"
-import App from "./App"
-import QueryProvider from "./query-client"
+import { NuqsReactAdaptor } from "@incmix/ui/data-table";
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
+import QueryProvider from "./query-client";
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
@@ -13,5 +13,5 @@ ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
         <App />
       </NuqsReactAdaptor>
     </QueryProvider>
-  </React.StrictMode>
-)
+  </React.StrictMode>,
+);
