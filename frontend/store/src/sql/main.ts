54d155cbd3a8a627e15d1eef842168593f7ccfd1
naymurdev <naymurrahman178@gmail.com>
2025-02-27 10:50:14 +0600

    feat: complete Grouplist widget (#61) and Property sheet (#62)

--- frontend/store/src/sql/main.ts
+++ frontend/store/src/sql/main.ts
@@ -9,7 +9,7 @@ import {
   taskSchemaLiteral,
 } from "./types"
 
-import { getRxStorageIndexedDB } from "rxdb-premium/plugins/storage-indexeddb"
+// import { getRxStorageIndexedDB } from "rxdb-premium/plugins/storage-indexeddb"
 import { RxDBMigrationSchemaPlugin } from "rxdb/plugins/migration-schema"
 import { RxDBUpdatePlugin } from "rxdb/plugins/update"
 import { wrappedValidateAjvStorage } from "rxdb/plugins/validate-ajv"
@@ -26,31 +26,31 @@ if (import.meta.env.MODE === "development") {
 // const sqliteModule = await SQLiteESMFactory()
 // const sqlite3 = SQLite.Factory(sqliteModule)
 
-const storage = wrappedValidateAjvStorage({ storage: getRxStorageIndexedDB() })
+// const storage = wrappedValidateAjvStorage({ storage: getRxStorageIndexedDB() })
 
-export const database = await createRxDatabase<TaskCollections>({
-  storage,
-  name: "incmix-db",
+// export const database = await createRxDatabase<TaskCollections>({
+//   storage,
+//   name: "incmix-db",
 
-  // getRxStorageSharedWorker({
-  //   //   /**
-  //   //    * Contains any value that can be used as parameter
-  //   //    * to the SharedWorker constructor of thread.js
-  //   //    * Most likely you want to put the path to the shared-worker.js file in here.
-  //   //    *
-  //   //    * @link https://developer.mozilla.org/en-US/docs/Web/API/SharedWorker?retiredLocale=de
-  //   //    */
+// getRxStorageSharedWorker({
+//   //   /**
+//   //    * Contains any value that can be used as parameter
+//   //    * to the SharedWorker constructor of thread.js
+//   //    * Most likely you want to put the path to the shared-worker.js file in here.
+//   //    *
+//   //    * @link https://developer.mozilla.org/en-US/docs/Web/API/SharedWorker?retiredLocale=de
+//   //    */
 
-  //   workerInput: () =>
-  //     new SharedWorker(new URL("./worker.ts", import.meta.url), {
-  //       type: "module",
-  //       // credentials: "include",
-  //     }),
-  // }),
-})
+//   workerInput: () =>
+//     new SharedWorker(new URL("./worker.ts", import.meta.url), {
+//       type: "module",
+//       // credentials: "include",
+//     }),
+// }),
+// })
 
-database.addCollections({
-  tasks: { schema: taskSchemaLiteral, autoMigrate: true },
-  columns: { schema: columnSchemaLiteral, autoMigrate: true },
-  projects: { schema: projectSchemaLiteral, autoMigrate: true },
-})
+// database.addCollections({
+//   tasks: { schema: taskSchemaLiteral, autoMigrate: true },
+//   columns: { schema: columnSchemaLiteral, autoMigrate: true },
+//   projects: { schema: projectSchemaLiteral, autoMigrate: true },
+// })
