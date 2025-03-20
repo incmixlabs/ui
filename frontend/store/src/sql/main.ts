99d79d33ccf795bc8866ef9d3480d6581f42d7ce
maan1996 <jsprtmnn@gmail.com>
2025-03-20 18:30:26 +0530

    feat: add rxdb replication endpoints for tasks page

--- frontend/store/src/sql/main.ts
+++ frontend/store/src/sql/main.ts
@@ -4,53 +4,79 @@ import { addRxPlugin, createRxDatabase } from "rxdb"
 import { RxDBDevModePlugin } from "rxdb/plugins/dev-mode"
 import {
   type TaskCollections,
+  type TaskDocType,
   columnSchemaLiteral,
   projectSchemaLiteral,
   taskSchemaLiteral,
 } from "./types"
 
+import { API } from "@incmix/utils/env"
 import { getRxStorageIndexedDB } from "rxdb-premium/plugins/storage-indexeddb"
 import { RxDBMigrationSchemaPlugin } from "rxdb/plugins/migration-schema"
+import { replicateRxCollection } from "rxdb/plugins/replication"
 import { RxDBUpdatePlugin } from "rxdb/plugins/update"
 import { wrappedValidateAjvStorage } from "rxdb/plugins/validate-ajv"
-
 addRxPlugin(RxDBUpdatePlugin)
 addRxPlugin(RxDBMigrationSchemaPlugin)
 
 if (import.meta.env.MODE === "development") {
   addRxPlugin(RxDBDevModePlugin)
 }
-// import * as SQLite from "wa-sqlite"
-// import SQLiteESMFactory from "wa-sqlite/dist/wa-sqlite.mjs"
-
-// const sqliteModule = await SQLiteESMFactory()
-// const sqlite3 = SQLite.Factory(sqliteModule)
 
 const storage = wrappedValidateAjvStorage({ storage: getRxStorageIndexedDB() })
 
 export const database = await createRxDatabase<TaskCollections>({
   storage,
   name: "incmix-db",
-
-  // getRxStorageSharedWorker({
-  //   //   /**
-  //   //    * Contains any value that can be used as parameter
-  //   //    * to the SharedWorker constructor of thread.js
-  //   //    * Most likely you want to put the path to the shared-worker.js file in here.
-  //   //    *
-  //   //    * @link https://developer.mozilla.org/en-US/docs/Web/API/SharedWorker?retiredLocale=de
-  //   //    */
-
-  //   workerInput: () =>
-  //     new SharedWorker(new URL("./worker.ts", import.meta.url), {
-  //       type: "module",
-  //       // credentials: "include",
-  //     }),
-  // }),
 })
 
-database.addCollections({
+await database.addCollections({
   tasks: { schema: taskSchemaLiteral, autoMigrate: true },
   columns: { schema: columnSchemaLiteral, autoMigrate: true },
   projects: { schema: projectSchemaLiteral, autoMigrate: true },
 })
+
+const BFF_API_URL: string = import.meta.env["VITE_BFF_API_URL"] || ""
+const TASKS_API_URL = `${BFF_API_URL}${API.TASKS}`
+
+if (database.tasks) {
+  replicateRxCollection<TaskDocType, { updatedAt: number; id: string }>({
+    collection: database.tasks,
+    replicationIdentifier: "tasks",
+    live: false,
+    push: {
+      async handler(changeRows) {
+        console.log("replicating tasks")
+        const rawResponse = await fetch(`${TASKS_API_URL}/sync/push`, {
+          method: "POST",
+          credentials: "include",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify(changeRows),
+        })
+        const conflictsArray = await rawResponse.json()
+        return conflictsArray
+      },
+    },
+    pull: {
+      async handler(checkpointOrNull, _batchSize) {
+        console.log(checkpointOrNull)
+        const checkPoint = checkpointOrNull ? checkpointOrNull.updatedAt : 0
+
+        const response = await fetch(
+          `${TASKS_API_URL}/sync/pull?lastPulledAt=${checkPoint}`,
+          {
+            method: "POST",
+            credentials: "include",
+          }
+        )
+        const data = await response.json()
+        return {
+          documents: data.documents,
+          checkpoint: data.checkpoint,
+        }
+      },
+    },
+  })
+}
