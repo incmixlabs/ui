d3f964345ab4f7405da3c4866c7698b1d46e5cdd
naymurdev <naymurrahman178@gmail.com>
2025-03-24 20:17:05 +0600

    feat(note): complete tiptap integration and the ui

---
+++ frontend/ui/src/components/notes/data.tsx
@@ -0,0 +1,82 @@
+export interface INote {
+  id: string
+  title: string
+  content: string
+  date: string
+  starred: boolean
+}
+
+export const notesData = [
+  {
+    id: "1",
+    title: "The title of a note",
+    content:
+      "Lorem ipsum dolor sit amet, ultricies adipisci consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquo commodo consequat. Duis aute irure.",
+    date: "12 June, 2023",
+    starred: true,
+  },
+  {
+    id: "2",
+    title: "The title of a note",
+    content:
+      "Lorem ipsum dolor sit amet, ultricies adipisci consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquo commodo consequat. Duis aute irure.",
+    date: "12 June, 2023",
+    starred: false,
+  },
+  {
+    id: "3",
+    title: "The title of a note",
+    content:
+      "Lorem ipsum dolor sit amet, ultricies adipisci consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquo commodo consequat. Duis aute irure.",
+    date: "12 June, 2023",
+    starred: true,
+  },
+  {
+    id: "4",
+    title: "The title of a note",
+    content:
+      "Lorem ipsum dolor sit amet, ultricies adipisci consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquo commodo consequat. Duis aute irure.",
+    date: "12 June, 2023",
+    starred: false,
+  },
+  {
+    id: "5",
+    title: "The title of a note",
+    content:
+      "Lorem ipsum dolor sit amet, ultricies adipisci consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquo commodo consequat. Duis aute irure.",
+    date: "12 June, 2023",
+    starred: false,
+  },
+  {
+    id: "6",
+    title: "The title of a note",
+    content:
+      "Lorem ipsum dolor sit amet, ultricies adipisci consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquo commodo consequat. Duis aute irure.",
+    date: "12 June, 2023",
+    starred: true,
+  },
+  {
+    id: "7",
+    title: "The title of a note",
+    content:
+      "Lorem ipsum dolor sit amet, ultricies adipisci consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquo commodo consequat. Duis aute irure.",
+    date: "12 June, 2023",
+    starred: false,
+  },
+  {
+    id: "8",
+    title: "The title of a note",
+    content:
+      "Lorem ipsum dolor sit amet, ultricies adipisci consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquo commodo consequat. Duis aute irure.",
+    date: "12 June, 2023",
+    starred: false,
+  },
+  {
+    id: "9",
+    title: "The title of a note",
+    content:
+      "Lorem ipsum dolor sit amet, ultricies adipisci consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquo commodo consequat. Duis aute irure.",
+    date: "12 June, 2023",
+    starred: false,
+  },
+]
