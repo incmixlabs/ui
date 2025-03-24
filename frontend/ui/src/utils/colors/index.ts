767161ec087a9f0c1fcbdd1cc8aa76ad917438e0
naymurdev <naymurrahman178@gmail.com>
2025-03-24 12:06:59 +0600

    fix: update colors

--- frontend/ui/src/utils/colors/index.ts
+++ frontend/ui/src/utils/colors/index.ts
@@ -5,12 +5,19 @@ import { accentColors as radixAccentColors } from "@radix-ui/themes/src/props/co
 import { grayColors as radixGrayColors } from "@radix-ui/themes/src/props/color.prop.js"
 
 // Define custom task colors
-export const dashboardColors = ["color1", "color2", "color3", "color4"] as const
+export const dashboardColors = [
+  "color1",
+  "color2",
+  "color3",
+  "color4",
+  "color5",
+] as const
 export const dashboardColorValues = {
   color1: "#FFCC33", // Previously "done"
   color2: "#3366FF", // Previously "hold"
   color3: "#FF9D66", // Previously "ongoing"
   color4: "#f2f4f7", // Previously "track"
+  color5: "#4f46e5",
 }
 
 export const accentColors = radixAccentColors
