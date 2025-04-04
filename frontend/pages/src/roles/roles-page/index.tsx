e45318534f4274cd984f35387fcef4fc8eff093e
maan1996 <jsprtmnn@gmail.com>
2025-04-04 20:46:05 +0530

    feat: permissions ui backend wip

--- frontend/pages/src/roles/roles-page/index.tsx
+++ frontend/pages/src/roles/roles-page/index.tsx
@@ -1,4 +1,4 @@
-import { Button, Flex, Tabs } from "@incmix/ui"
+import { Button, Flex, Tabs, toast } from "@incmix/ui"
 import {
   Table,
   TableBody,
@@ -20,12 +20,25 @@ import React, { useState } from "react"
 import { getColumns } from "./columns"
 import { groupedPermissions, permissionsWithRoles } from "./mock"
 import type { RoleWithPermissionsWithSubrows } from "./types"
+import { useMutation, useQuery, useQueryClient } from "@tanstack/react-query"
+import {
+  type Change,
+  getRolesPermissions,
+  updateRolesPermissions,
+} from "./actions"
 const RolesPage = () => {
   const [expandedRows, setExpandedRows] = useState<ExpandedState>({})
-  console.log(permissionsWithRoles)
-  const columns = getColumns()
+
+  const { data } = useQuery({
+    queryKey: ["roles-permissions"],
+    queryFn: getRolesPermissions,
+  })
+
+  const [changes, setChanges] = useState<Change[]>([])
+
+  const columns = getColumns(data?.roles ?? [])
   const table = useReactTable<RoleWithPermissionsWithSubrows>({
-    data: groupedPermissions,
+    data: data?.permissions ?? [],
     columns,
     getCoreRowModel: getCoreRowModel(),
     getExpandedRowModel: getExpandedRowModel(),
@@ -35,21 +48,41 @@ const RolesPage = () => {
     onExpandedChange: setExpandedRows,
     getSubRows: (row) => row.subRows,
   })
+
+  const queryClient = useQueryClient()
+
+  const mutation = useMutation({
+    mutationFn: updateRolesPermissions,
+    onSuccess: async () => {
+      await queryClient.invalidateQueries({ queryKey: ["roles-permissions"] })
+      toast.success("Roles permissions updated")
+    },
+    onError: () => {
+      toast.error("Failed to update roles permissions")
+    },
+  })
+
+  const handleSaveChanges = () => {
+    mutation.mutate(changes)
+  }
+
   return (
     <DashboardLayout breadcrumbItems={[{ label: "Roles", url: "/roles" }]}>
       <Flex className="px-2 pt-4 pb-4 2xl:p-2" direction="column" gap="3">
         <Flex className="px-6" justify="between" gap="2" align="center">
-          <Tabs.Root defaultValue="permissions">
+          {/* <Tabs.Root defaultValue="permissions">
             <Tabs.List>
               <Tabs.Trigger value="users">Users</Tabs.Trigger>
               <Tabs.Trigger value="permissions">Permissions</Tabs.Trigger>
             </Tabs.List>
-          </Tabs.Root>
+          </Tabs.Root> */}
           <Flex justify="end" gap="2" align="center">
             <Button variant="outline">
               <PlusCircleIcon /> Add New Role
             </Button>
-            <Button variant="soft">Save Changes</Button>
+            <Button variant="soft" onClick={handleSaveChanges}>
+              Save Changes
+            </Button>
           </Flex>
         </Flex>
         <Table>
