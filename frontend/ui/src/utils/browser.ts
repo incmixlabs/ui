f0d3f00b4fcca3f83c60041e00b7b8c3a5fa6a8e
uma-uc <uma.apple123@gmail.com>
2025-04-06 16:34:01 -0700

    chore: add once

--- frontend/ui/src/utils/browser.ts
+++ frontend/ui/src/utils/browser.ts
@@ -1,6 +1,19 @@
 /* Returns `true` if a `Safari` browser.
  * Returns `true` if the browser is running on iOS (they are all Safari).
  * */
+function once<T extends (...args: any[]) => any>(func: T): (...args: Parameters<T>) => ReturnType<T> | undefined {
+  let executed = false;
+  let result: ReturnType<T> | undefined;
+
+  return function (...args: Parameters<T>): ReturnType<T> | undefined {
+    if (!executed) {
+      executed = true;
+      result = func(...args);
+      return result;
+    }
+    return undefined;
+  };
+}
 export const isSafari = once(function isSafari(): boolean {
   if (process.env.NODE_ENV === "test") {
     return false
