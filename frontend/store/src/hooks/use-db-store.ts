932390d5ea229f8bdba18bbadb1356db09fd64af
uma-uc <94931629+uma-uc@users.noreply.github.com>
2025-06-25 18:23:13 -0700

    Update frontend/store/src/hooks/use-db-store.ts
    Co-authored-by: coderabbitai[bot] <136622811+coderabbitai[bot]@users.noreply.github.com>

--- frontend/store/src/hooks/use-db-store.ts
+++ frontend/store/src/hooks/use-db-store.ts
@@ -2,15 +2,20 @@
 import { useEffect, useState } from "react"
 
 const useStore = <T, F>(
-  store: (callback: (state: T) => unknown) => unknown,
+  store: (callback: (state: T) => F) => () => void,
   callback: (state: T) => F
 ) => {
-  const result = store(callback) as F
   const [data, setData] = useState<F>()
 
   useEffect(() => {
-    setData(result)
-  }, [result])
+    const unsubscribe = store((state) => {
+      const result = callback(state)
+      setData(result)
+      return result
+    })
+
+    return unsubscribe
+  }, [store, callback])
 
   return data
 }
