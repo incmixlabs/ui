ee49dd216fe8b1b3f8ff713e1c65c86c6beef664
uma-uc <94931629+uma-uc@users.noreply.github.com>
2025-07-25 10:00:27 -0700

    chore: remove-dead-code (#413)

--- frontend/ui/src/preset.ts
+++ frontend/ui/src/preset.ts
@@ -13,46 +13,6 @@ import { createPlugin } from "windy-radix-palette"
 import windyTypography from "windy-radix-typography"
 
 const colors = createPlugin()
-const _config = {
-  darkMode: ["class"],
-  content: ["./*.{ts,tsx}"],
-  theme: {
-    extend: {
-      keyframes: {
-        "accordion-down": {
-          from: {
-            height: "0",
-          },
-          to: {
-            height: "var(--radix-accordion-content-height)",
-          },
-        },
-        "accordion-up": {
-          from: {
-            height: "var(--radix-accordion-content-height)",
-          },
-          to: {
-            height: "0",
-          },
-        },
-        "collapsible-down": {
-          from: { height: "0" },
-          to: { height: "var(--radix-collapsible-content-height)" },
-        },
-        "collapsible-up": {
-          from: { height: "var(--radix-collapsible-content-height)" },
-          to: { height: "0" },
-        },
-      },
-      animation: {
-        "accordion-down": "accordion-down 0.2s ease-out",
-        "accordion-up": "accordion-up 0.2s ease-out",
-        "collapsible-down": "collapsible-down 200ms ease-in-out",
-        "collapsible-up": "collapsible-up 200ms ease-in-out",
-      },
-    },
-  },
-} satisfies Config
 
 // Functions to generate RadixUI Theme tags for tailwind.
 // See [https://blog.soards.me/posts/radix-colors-with-tailwind/] for more details.
