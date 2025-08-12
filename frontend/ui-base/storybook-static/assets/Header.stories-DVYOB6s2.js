65de3c1d6211cecb56eba52e9e4aafaa9e9f4047
uma-uc <uma.apple123@gmail.com>
2025-08-12 12:28:05 -0700

    chore: storybook static

--- frontend/ui-base/storybook-static/assets/Header.stories-DVYOB6s2.js
+++
@@ -1,7 +0,0 @@
-import{H as t}from"./Header-D3FKQDLo.js";import"./jsx-runtime-D_zvdyIk.js";import"./button-jOk6Qw9K.js";import"./button-ClCv9eVs.js";import"./high-contrast.prop-DN4VqJ5o.js";import"./iframe-DsuaOdjx.js";import"./preload-helper-D9Z9MdNV.js";import"./index-CVlKO6je.js";import"./index-BW54sZL3.js";import"./base-button-Dyf4vkZD.js";import"./flex-bym-5aeO.js";import"./radius.prop-BFb5uVoY.js";const{fn:r}=__STORYBOOK_MODULE_TEST__,L={title:"Example/Header",component:t,tags:["autodocs"],parameters:{layout:"fullscreen"},args:{onLogin:r(),onLogout:r(),onCreateAccount:r()}},e={args:{user:{name:"Jane Doe"}}},o={};e.parameters={...e.parameters,docs:{...e.parameters?.docs,source:{originalSource:`{
-  args: {
-    user: {
-      name: "Jane Doe"
-    }
-  }
-}`,...e.parameters?.docs?.source}}};o.parameters={...o.parameters,docs:{...o.parameters?.docs,source:{originalSource:"{}",...o.parameters?.docs?.source}}};const O=["LoggedIn","LoggedOut"];export{e as LoggedIn,o as LoggedOut,O as __namedExportsOrder,L as default};
--- frontend/ui-base/storybook-static/assets/index-CVlKO6je.js
+++
@@ -1,1 +0,0 @@
-import{g as r}from"./iframe-DsuaOdjx.js";import{r as o}from"./index-BW54sZL3.js";var t=o();const m=r(t);export{m as R,t as r};
